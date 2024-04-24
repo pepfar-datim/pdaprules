a5a786703785f70eabd6e4e90022d92e95a3a9ca
jbales <jbales@baosystems.com>
2024-04-24 19:53:01 +0000

    Basis of functions set, just need to add warnings and safety nets.

--- R/s3_filter_PAW.R
+++ R/s3_filter_PAW.R
@@ -1,12 +1,14 @@
-s3_filter_PAW <- function(category = "MER", subcategory = NULL, metadata = FALSE, country = NULL) {
+s3_filter_PAW <- function(bucketlist = NULL, category = "MER", subcategory = NULL,
+                          metadata = FALSE, country = NULL) {
   
+  #bucketlist is derived from s3_list_bucket_items
   #category options ^MER|^Financial|^Narratives
   #subcategory Site_Recent, Site_Historic, PSNU_Recent, PSNU_Historic, OU_Recent, OU_Historic
   #metadata include true or false
   #Country specific country to sort for 
   
   #List bucket choices
-  choices <- s3_list_bucket_items(bucket = Sys.getenv("TEST_BUCKET"))
+  choices <- bucketlist
   
   #Sort bucket based upon desired category
   choices <- choices[grepl(category, choices$path_names), ]
@@ -26,8 +28,11 @@ s3_filter_PAW <- function(category = "MER", subcategory = NULL, metadata = FALSE
     choices <- choices[grepl(country, choices$path_names), ]
   }
   
-  # reset row names
+  #Reset row names
   rownames(choices) <- NULL
+  
+  #Turn into List
+  choices <- as.list(choices$path_names)
   
   choices
   
--- R/s3_read_PAW.R
+++ R/s3_read_PAW.R
@@ -1,78 +1,95 @@
-# #read the xlsx from the s3 bucket
-ndf <- aws.s3::s3read_using(FUN = read_excel,
-                            bucket = paste(server,".pepfar.data.extracts-971c9206e248",sep=""),
-                            object = narFiles$path_names[grepl("Narratives.xlsx", narFiles$path_names)])
-# #narFiles$path_names[3]) # Wed Nov 30 14:25:30 2022 In case above doesn't work this does
+# library(dplyr)
 
-#WORKS but need to delim by |
-ndf <- aws.s3::s3read_using(FUN = read.csv,
-                            bucket = Sys.getenv("TEST_BUCKET"),
-                            object = test)
-#Works
-s3_object <- s3$get_object(Bucket = Sys.getenv("TEST_BUCKET"), Key = test)
-
-#Works
-s3_object_body <- s3_object$Body
-
-#Works
-file_name2 <- paste0(getwd(), "/", test)
-
-#Doesn't work because no file exists already. 
-con <- file(file_name2, "wb")
-writeBin(s3_object_body, con = con)
-close(con) 
-
-#Works
-file_name2 <- s3_object_body %>%
-  rawToChar %>%
-  read.csv(text = ., sep = "|")
-
-
-s3_read_PAW<- function(path, locally = TRUE) {
-  #########################################################################
-  #Make original connection
-  s3 <- paws::s3()
+s3_read_PAW<- function(path) {
   
-  s3_object <-
-    tryCatch({
-      s3$get_object(Bucket = Sys.getenv("TEST_BUCKET"),
-                    Key = path)
-    },
-    error = function(e) {
-      interactive_warning("Could not retreive support file data from S3")
-      return(NULL)
-    })
+  fileList <- path 
   
-  s3_object_body <- s3_object$Body
-  #########################################################################
-  if (locally==T) {
-    file_name2 <- paste0(getwd(), "/", path)
-    
-    if (file.exists(file_name2)) {
-      unlink(file_name2)
+  allMyData <- lapply(
+    fileList,
+    function(f) {
+      aws.s3::s3read_using(
+        FUN = readr::read_delim, "|", escape_double = FALSE,
+        trim_ws = TRUE, col_types = readr::cols(.default = readr::col_character()
+        ),
+        object = f,
+        bucket = Sys.getenv("TEST_BUCKET"),
+      ) 
     }
-    
-    tryCatch({con <- file(file_name2, "wb")
-    writeBin(s3_object_body, con = con)
-    close(con)    },
-    error = function(e) {
-      interactive_warning("Could not save support file locally")
-      return(NULL)
-      
-    })
-    
-    futile.logger::flog.info(paste0("Retreived support file to ", file_name2))
-    if (!file.exists(file_name2)) {
-      stop("Could not retreive support file.")
-    }
-    #########################################################################    
-  } else{
-    file_name2 <- s3_object_body %>%
-      rawToChar %>%
-      read.csv(text = ., sep = "|")
-    
-    futile.logger::flog.info("Retreived support file in memory")
-  }
-  
-  return(file_name2)
+  ) %>% 
+    bind_rows()
 }
+
+
+
+
+# ######################## ALT OPTION ########################################################
+# #Works
+# s3_object <- s3$get_object(Bucket = Sys.getenv("TEST_BUCKET"), Key = test)
+# 
+# #Works
+# s3_object_body <- s3_object$Body
+# 
+# #Works
+# file_name2 <- paste0(getwd(), "/", test)
+# 
+# #Doesn't work because no file exists already. 
+# con <- file(file_name2, "wb")
+# writeBin(s3_object_body, con = con)
+# close(con) 
+# 
+# #Works
+# file_name2 <- s3_object_body %>%
+#   rawToChar %>%
+#   read.csv(text = ., sep = "|")
+# 
+# 
+# 
+# 
+# s3_read_PAW<- function(path, locally = TRUE) {
+#   #########################################################################
+#   #Make original connection
+#   s3 <- paws::s3()
+#   
+#   s3_object <-
+#     tryCatch({
+#       s3$get_object(Bucket = Sys.getenv("TEST_BUCKET"),
+#                     Key = path)
+#     },
+#     error = function(e) {
+#       interactive_warning("Could not retreive support file data from S3")
+#       return(NULL)
+#     })
+#   
+#   s3_object_body <- s3_object$Body
+#   #########################################################################
+#   if (locally==T) {
+#     file_name2 <- paste0(getwd(), "/", path)
+#     
+#     if (file.exists(file_name2)) {
+#       unlink(file_name2)
+#     }
+#     
+#     tryCatch({con <- file(file_name2, "wb")
+#     writeBin(s3_object_body, con = con)
+#     close(con)},
+#     error = function(e) {
+#       interactive_warning("Could not save support file locally")
+#       return(NULL)
+#       
+#     })
+#     
+#     futile.logger::flog.info(paste0("Retreived support file to ", file_name2))
+#     if (!file.exists(file_name2)) {
+#       stop("Could not retreive support file.")
+#     }
+#     #########################################################################    
+#   } else{
+#     file_name2 <- s3_object_body %>%
+#       rawToChar %>%
+#       read.csv(text = ., sep = "|")
+#     
+#     futile.logger::flog.info("Retreived support file in memory")
+#   }
+#   
+#   return(file_name2)
+# }
